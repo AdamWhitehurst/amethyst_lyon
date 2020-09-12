5af4a80a3ccd1bd0ec8fce7cb9bde731ec0745a0
Adam Whitehurst <Adam.Whitehurst@live.com>
2020-09-12 15:41:54 -0700

    switch amethyst_error to amethyst

--- src/lib.rs
+++ src/lib.rs
@@ -1,35 +1,26 @@
-//! Description: 
-//! 
-//! A custom render plugin for meshes created with Lyon. 
-//! 
+//! Description:
+//!
+//! A custom render plugin for meshes created with Lyon.
+//!
 //! Copyright © 2020 Benedict Gaster. All rights reserved.
-//! 
+//!
 pub mod pass;
 pub mod utils;
 
 use amethyst::{
-    core::{
-        ecs::{
-            DispatcherBuilder, World,
-        },
-    },
+    core::ecs::{DispatcherBuilder, World},
     prelude::*,
     renderer::{
         bundle::{RenderOrder, RenderPlan, RenderPlugin, Target},
-        rendy::{
-            factory::Factory,
-            graph::{
-                render::{RenderGroupDesc},
-            },
-        },
+        rendy::{factory::Factory, graph::render::RenderGroupDesc},
         types::Backend,
     },
 };
 
-use amethyst_error::Error;
+use amethyst::Error;
 
-use crate::utils::{Mesh, ActiveMesh};
-use crate::pass::{DrawLyonDesc};
+use crate::pass::DrawLyonDesc;
+use crate::utils::{ActiveMesh, Mesh};
 
 #[derive(Default, Debug)]
 pub struct RenderLyon {}
